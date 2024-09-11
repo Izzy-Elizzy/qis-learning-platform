a39de7f38ec7eeaacdd6029492b6aff42548d9b0
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-09-11 10:58:26 -0400

    Working Demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <iframe
+        className="video"
+          src="https://www.youtube.com/embed/GSsElSQgMbU?si=0rRDod-ghZg45FIb&amp;start=1854"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+
+        <iframe className="chatbot"
+        src="http://137.184.241.149:3000/"
+        title="Gemini Chat Bot"
+        frameborder="0"
+        ></iframe>
+
+        <div className="launch-jupyter"  onClick={() => handleLaunchClick()}>
+          <p>
+            Launch Assignment
+          </p>
+        </div>
       </header>
     </div>
   );
+}
+
+function handleLaunchClick() {
+  const jupyterHubUrl = "http://137.184.241.149"; // Replace with the IP address
+  window.open(jupyterHubUrl, '_blank', 'noopener,noreferrer'); 
 }
 
 export default App;
